622c15e493eeb7cb50b1c71962178d7b085eb414
paulaperazzo <mpps@cin.ufpe.br>
2025-06-26 18:44:10 -0300

    feat/spreadsheet: json load correction

--- database/sheet_connection.py
+++ database/sheet_connection.py
@@ -1,3 +1,4 @@
+import textwrap
 import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
@@ -8,9 +9,15 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 @st.cache_data  # opcional
 def get_sheet_df(spreadsheet_id: str, sheet_name: str) -> pd.DataFrame:
-    info = json.loads(st.secrets["google_sheets"]["service_account_info"])
-
-    creds = Credentials.from_service_account_file(info, scopes=SCOPES)
+    raw = st.secrets["google_json"]["service_account_info"]
+    print(raw)
+    cleaned = textwrap.dedent(raw).strip()  # tira indentação e espaços em volta
+    cleaned = cleaned.replace("\n", "\\n")
+    print(cleaned)
+    
+    # 2) carrega o JSON
+    info = json.loads(cleaned)
+    creds = Credentials.from_service_account_info(info, scopes=SCOPES)
 
     client = gspread.authorize(creds)
     sheet = client.open_by_key(spreadsheet_id)
@@ -18,3 +25,4 @@ def get_sheet_df(spreadsheet_id: str, sheet_name: str) -> pd.DataFrame:
     records = ws.get_all_records()
 
     return pd.DataFrame.from_records(records)
+
